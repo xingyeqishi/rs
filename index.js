495c8f557208acd8d7af13e96f11633635773248
sunxiaoshen <sunxiaoshen@meituan.com>
2014-06-10 19:30:14 +0800

    fix

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var express = require('express');
 var app = express();
-require('./lib/server.js')(app);
-app.listen(3000);
+server = require('http').Server(app), 
+io = require('socket.io')(server);
+require('./lib/server.js')(app, io);
+io.on('connection', function(socket){
+    socket.on('event', function(data){});
+    socket.on('disconnect', function(){});
+});
+server.listen(3000);
